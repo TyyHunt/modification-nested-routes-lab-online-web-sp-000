7a7c10d491f4cbc2153f33d054ea239d59383997
Tyler Hunt <tyhunt04@gmail.com>
2019-05-09 17:13:45 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -50,8 +50,8 @@ class SongsController < ApplicationController
   def edit
     if params[:artist_id]
       @artist = Artist.find_by(id: params[:artist_id])
-      if @artist.nil?
-        redirect_to artists_path
+      if !@artist.nil?
+        @song = @artist.songs.find_by(id: params[:id])
       else
         @song = @artist.songs.find_by(id: params[:id])
       end
